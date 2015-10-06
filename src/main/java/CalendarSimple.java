f47a2252c2ec9c2174e73cf02019b4866c4f98c9
employee <edgarz@interlink-ua.com>
2015-10-06 14:59:50 +0300

    Testing

--- src/main/java/CalendarSimple.java
+++ src/main/java/CalendarSimple.java
@@ -1,5 +1,6 @@
 public class CalendarSimple {
     public static void main(String[] args) {
+        System.out.println("Testing");
         MyCalendar al = new MyCalendar(MyCalendar.Style.AMERICAN);
         al.print();
     }
